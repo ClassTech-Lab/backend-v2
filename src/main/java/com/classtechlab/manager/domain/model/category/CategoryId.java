b06cf860348334d30b48dcf2736cef1458732249
keyhoh <iheyhoh+github@gmail.com>
2020-11-14 16:13:49 +0900

    update: equality of id

--- src/main/java/com/classtechlab/manager/domain/model/category/CategoryId.java
+++ src/main/java/com/classtechlab/manager/domain/model/category/CategoryId.java
@@ -12,4 +12,19 @@ public class CategoryId {
     public CategoryId() {
         this(UUID.randomUUID());
     }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final CategoryId that = (CategoryId) o;
+
+        return value.equals(that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
 }
--- src/main/java/com/classtechlab/manager/domain/model/keyword/KeywordId.java
+++ src/main/java/com/classtechlab/manager/domain/model/keyword/KeywordId.java
@@ -12,4 +12,19 @@ public class KeywordId {
     public KeywordId() {
         this(UUID.randomUUID());
     }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final KeywordId keywordId = (KeywordId) o;
+
+        return value.equals(keywordId.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
 }
