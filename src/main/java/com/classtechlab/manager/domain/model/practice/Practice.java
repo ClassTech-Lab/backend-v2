0b313d9cd2f7728db657203e0fbe876e49df733c
keyhoh <iheyhoh+github@gmail.com>
2020-12-08 23:01:01 +0900

    update: frameworkとtoolを更新

--- src/main/java/com/classtechlab/manager/domain/model/practice/Practice.java
+++ src/main/java/com/classtechlab/manager/domain/model/practice/Practice.java
@@ -1,20 +1,17 @@
 package com.classtechlab.manager.domain.model.practice;
 
 import com.classtechlab.manager.domain.model.practice.content.PracticeContent;
-import com.classtechlab.manager.domain.model.school.SchoolId;
 
 public class Practice {
     private final PracticeId id;
     private final PracticeContent content;
-    private final SchoolId schoolId;
 
-    public Practice(final PracticeId id, final PracticeContent content, final SchoolId schoolId) {
+    public Practice(final PracticeId id, final PracticeContent content) {
         this.id = id;
         this.content = content;
-        this.schoolId = schoolId;
     }
 
-    public Practice(final PracticeContent practiceContent, final SchoolId schoolId) {
-        this(new PracticeId(), practiceContent, schoolId);
+    public Practice(final PracticeContent content) {
+        this(new PracticeId(), content);
     }
 }
