ea8334aec172b30354ae96e16833a422c1b7c54b
keyhoh <iheyhoh+github@gmail.com>
2020-12-31 11:54:42 +0900

    update: UUIDを固定

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,8 @@
-create extension if NOT EXISTS "uuid-ossp";
-insert into public.tool (id, name)
-values (uuid_generate_v4(), 'tool 1'),
-       (uuid_generate_v4(), 'tool 2');
+INSERT INTO public.tool (id, name) VALUES ('99dded1c-d320-4541-b3f8-d098efe3d567', 'tool 1');
+INSERT INTO public.tool (id, name) VALUES ('c879e171-5518-4bea-9176-10c0d20fd43b', 'tool 2');
 
-insert into public.keyword (id, name)
-values (uuid_generate_v4(), 'keyword 1'),
-       (uuid_generate_v4(), 'keyword 2');
+INSERT INTO public.keyword (id, name) VALUES ('d8bfd246-7e5a-415e-b56e-22741d7a4379', 'keyword 1');
+INSERT INTO public.keyword (id, name) VALUES ('f650bf49-a6ae-4207-88cd-5afc6823438c', 'keyword 2');
 
-insert into public.category (id, name)
-values (uuid_generate_v4(), 'category 1'),
-       (uuid_generate_v4(), 'category 2');
+INSERT INTO public.category (id, name) VALUES ('cd21f6a4-5d75-45f7-a8c0-9eae53ba30a4', 'category 1');
+INSERT INTO public.category (id, name) VALUES ('1426640e-6bca-4d91-bd5b-6891a35d17fd', 'category 2');
