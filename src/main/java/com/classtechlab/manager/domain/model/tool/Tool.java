987c8f4ea20b497e501eb37a4adfc35c7a717c82
keyhoh <iheyhoh+github@gmail.com>
2021-01-01 12:53:07 +0900

    clean: 不要なコードを削除

--- src/main/java/com/classtechlab/manager/domain/model/tool/Tool.java
+++ src/main/java/com/classtechlab/manager/domain/model/tool/Tool.java
@@ -1,11 +1,9 @@
 package com.classtechlab.manager.domain.model.tool;
 
-import com.classtechlab.manager.domain.exception.IllegalArgumentException;
 import com.classtechlab.manager.domain.type.item.Identifiable;
 import com.classtechlab.manager.domain.type.name.Name;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import org.apache.commons.lang3.StringUtils;
 
 @JsonSerialize(using = ToolSerializer.class)
 @JsonDeserialize(using = ToolDeserializer.class)
@@ -44,27 +42,5 @@ public class Tool implements Identifiable<Tool> {
     @Override
     public boolean isEqualTo(final Tool other) {
         return this.id.isEqualTo(other.id);
-    }
-
-    public static class PlainObject {
-        private String id;
-        private String name;
-
-        private PlainObject() {
-        }
-
-        private PlainObject(final Tool tool) {
-            this.id = tool.id.string();
-            this.name = tool.name.string();
-        }
-
-        public Tool toTool(final ToolId id) throws IllegalArgumentException {
-            if (id == null || StringUtils.isBlank(this.name)) throw new IllegalArgumentException();
-            return new Tool(id, new Name(this.name));
-        }
-
-        public Tool toTool() throws IllegalArgumentException {
-            return this.toTool(new ToolId());
-        }
     }
 }
