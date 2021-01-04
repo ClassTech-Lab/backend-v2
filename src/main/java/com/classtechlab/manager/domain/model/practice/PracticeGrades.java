72fa2a5a5edf5f87511ae4a179731558fba1313a
keyhoh <iheyhoh+github@gmail.com>
2021-01-04 23:43:02 +0900

    update: 不要なコードを削除

--- src/main/java/com/classtechlab/manager/domain/model/practice/PracticeGrades.java
+++ src/main/java/com/classtechlab/manager/domain/model/practice/PracticeGrades.java
@@ -1,29 +1,29 @@
 package com.classtechlab.manager.domain.model.practice;
 
-import com.classtechlab.manager.domain.model.school.grade.GradeId;
+import com.classtechlab.manager.domain.model.school.grade.Grade;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class PracticeGrades {
     private final PracticeId practiceId;
-    private final List<GradeId> gradeIdList;
+    private final Set<Grade> grades;
 
-    public PracticeGrades(final PracticeId practiceId, final List<GradeId> gradeIdList) {
+    public PracticeGrades(final PracticeId practiceId, final Set<Grade> grades) {
         this.practiceId = practiceId;
-        this.gradeIdList = Collections.unmodifiableList(gradeIdList);
+        this.grades = Collections.unmodifiableSet(grades);
     }
 
-    PracticeGrades add(final GradeId gradeId) {
-        List<GradeId> gradeIdList = new ArrayList<>(this.gradeIdList);
-        gradeIdList.add(gradeId);
-        return new PracticeGrades(this.practiceId, gradeIdList);
+    PracticeGrades add(final Grade grade) {
+        Set<Grade> grades = new HashSet<>(this.grades);
+        grades.add(grade);
+        return new PracticeGrades(this.practiceId, grades);
     }
 
-    public PracticeGrades remove(final GradeId gradeId) {
-        final List<GradeId> gradeIdList = new ArrayList<>(this.gradeIdList);
-        gradeIdList.remove(gradeId);
-        return new PracticeGrades(this.practiceId, gradeIdList);
+    public PracticeGrades remove(final Grade grade) {
+        final Set<Grade> grades = new HashSet<>(this.grades);
+        grades.remove(grade);
+        return new PracticeGrades(this.practiceId, grades);
     }
 }
