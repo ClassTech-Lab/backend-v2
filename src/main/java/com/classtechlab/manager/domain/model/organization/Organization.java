ff4189a12322b4112b9a12cf5ae8b9142bde16c0
keyhoh <iheyhoh+github@gmail.com>
2021-01-06 20:51:03 +0900

    clean: departmentを追加

--- src/main/java/com/classtechlab/manager/domain/model/organization/Organization.java
+++ src/main/java/com/classtechlab/manager/domain/model/organization/Organization.java
@@ -15,7 +15,7 @@ public class Organization implements Identifiable<Organization> {
     private final Set<Type> types;
     private final Set<Department> departments;
 
-    private Organization(final OrganizationId id, final Set<Type> types, final Set<Department> departments, final ManagementBody managementBody) {
+    private Organization(final OrganizationId id, final ManagementBody managementBody, final Set<Type> types, final Set<Department> departments) {
         this.id = id;
         this.types = Collections.unmodifiableSet(types);
         this.departments = Collections.unmodifiableSet(departments);
@@ -23,7 +23,7 @@ public class Organization implements Identifiable<Organization> {
     }
 
     public Organization(final Set<Type> types, final ManagementBody managementBody, final Set<Department> departments) {
-        this(new OrganizationId(), types, departments, managementBody);
+        this(new OrganizationId(), managementBody, types, departments);
     }
 
     private OrganizationId id() {
@@ -33,13 +33,25 @@ public class Organization implements Identifiable<Organization> {
     public Organization add(final Type type) {
         final Set<Type> types = new HashSet<>(this.types);
         types.add(type);
-        return new Organization(this.id, types, this.departments, this.managementBody);
+        return new Organization(this.id(), this.managementBody, types, this.departments);
     }
 
     public Organization remove(final Type type) {
         final Set<Type> types = new HashSet<>(this.types);
         types.remove(type);
-        return new Organization(this.id, types, this.departments, this.managementBody);
+        return new Organization(this.id(), this.managementBody, types, this.departments);
+    }
+
+    public Organization add(final Department department) {
+        final Set<Department> departments = new HashSet<>(this.departments);
+        departments.add(department);
+        return new Organization(this.id(), this.managementBody, this.types, departments);
+    }
+
+    public Organization remove(final Department department) {
+        final Set<Department> departments = new HashSet<>(this.departments);
+        departments.remove(department);
+        return new Organization(this.id(), this.managementBody, this.types, departments);
     }
 
     boolean has(final Type typeId) {
