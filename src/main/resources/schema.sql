743d6ae392bd4b72dea64d32e6fc26de2834e630
keyhoh <iheyhoh+github@gmail.com>
2021-01-12 22:41:26 +0900

    fix: mapperファイルの指定を修正

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,20 +1,36 @@
-drop table if exists tool;
+drop table if exists public.tool;
 create table public.tool
 (
     id   uuid primary key,
     name varchar unique not null
 );
 
-drop table if exists keyword;
+drop table if exists public.keyword;
 create table public.keyword
 (
     id   uuid primary key,
     name varchar unique not null
 );
 
-drop table if exists category;
+drop table if exists public.category;
 create table public.category
 (
     id   uuid primary key,
     name varchar unique not null
+);
+
+drop table if exists public.organization;
+drop table if exists public.management_body;
+create table public.management_body
+(
+    id   uuid primary key,
+    name varchar unique not null
+);
+
+create table public.organization
+(
+    id                 uuid primary key,
+    name               varchar unique not null,
+    management_body_id uuid           not null,
+    foreign key (management_body_id) references public.management_body (id)
 );
