a524bdf20737aa08161653c1baf589beccdbcf57
keyhoh <iheyhoh+github@gmail.com>
2021-01-13 23:35:22 +0900

    clean: mapperクラスをpackage-privateに変更

--- src/main/java/com/classtechlab/manager/infrastructure/datasource/category/CategoryMapper.java
+++ src/main/java/com/classtechlab/manager/infrastructure/datasource/category/CategoryMapper.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Mapper;
 import java.util.List;
 
 @Mapper
-public interface CategoryMapper {
+interface CategoryMapper {
     void insert(final Category category);
 
     List<Category> findAll();
--- src/main/java/com/classtechlab/manager/infrastructure/datasource/keyword/KeywordMapper.java
+++ src/main/java/com/classtechlab/manager/infrastructure/datasource/keyword/KeywordMapper.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Mapper;
 import java.util.List;
 
 @Mapper
-public interface KeywordMapper {
+interface KeywordMapper {
     void insert(final Keyword keyword);
 
     List<Keyword> findAll();
